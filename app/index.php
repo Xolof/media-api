fb78acd699274558b5a6c08b78c2079f1d689030
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2021-03-19 07:36:33 +0100

    Move index.php to separate directory. Build URL dynamically. Add more comments

---
+++ app/index.php
@@ -0,0 +1,76 @@
+<?php
+/**
+* Get data from the Instagram Basic Display API.
+* Reference: https://developers.facebook.com/docs/instagram-basic-display-api/guides/getting-profiles-and-media
+* Olof Johansson
+* 2021-03-19
+*/
+
+// Turn on or off error reporting.
+error_reporting(0);
+// error_reporting(E_ALL);
+
+// Import the access token.
+$rawAccessToken = file_get_contents("../secret/long-lived-token.txt");
+
+// Strip token of newlines
+$accessToken = trim($rawAccessToken);
+
+// The API URL without query params.
+$baseUrl = "https://graph.instagram.com/me/media";
+
+// The data fields to request.
+// Reference: https://developers.facebook.com/docs/instagram-basic-display-api/reference/media#fields
+// NOTE: Can't find any field about likes. Perhaps this field is only available in the Instagram Graph API.
+$fields = [
+    "id",
+    "caption",
+    "media_type",
+    "media_url",
+    "permalink",
+    "thumbnail_url",
+    "timestamp",
+    "username"
+];
+
+// Construct the URL.
+$url = $baseUrl . "?fields=" . implode(",", $fields) . "&access_token=" . $accessToken;
+
+// Try to make a cURL request.
+try {
+    // Initialize a new session and return a cURL handle
+    // for use with the curl_setopt(), curl_exec(), and curl_close() functions.
+    $curlRequest = curl_init($url);
+
+    // Set CURLOPT_RETURNTRANSFER to true.
+    // Return the transfer as a string of the return value of curl_exec()
+    // instead of outputting it directly.
+    curl_setopt($curlRequest, CURLOPT_RETURNTRANSFER, true);
+
+    // Execute the cURL session.
+    $json = curl_exec($curlRequest);
+
+    if ($json === false) {
+        // If it fails, throw an exception.
+        throw new Exception(curl_error($curlRequest), curl_errno($curlRequest));
+    }
+
+    // Close the cURL session.
+    curl_close($curlRequest);
+} catch (Exception $e) {
+    // Print exception.
+    trigger_error(
+        sprintf(
+            'Curl failed with error #%d: %s',
+            $e->getCode(),
+            $e->getMessage()
+        ),
+        E_USER_ERROR
+    );
+}
+
+// Set header to send response as JSON.
+header('Content-Type: application/json');
+
+// Send response.
+echo $json;
